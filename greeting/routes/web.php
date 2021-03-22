3b753fb81b52d42e25d1313df7dfddbf24226c06
dotrinh81197 <dotrinh81197@users.noreply.github.com>
2021-03-22 10:20:07 +0700

    Thuc hanh Hello World

--- greeting/routes/web.php
+++ greeting/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/greeting/{name?}',  function ($name = null) {
+    if ($name) {
+        echo 'Hello ' . $name . '!';
+    } else {
+        echo 'Hello World !';
+    }
 });
