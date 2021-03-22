035bff4344c5a0dccfabd25342806838a32dfd60
dotrinh81197 <dotrinh81197@users.noreply.github.com>
2021-03-22 10:35:55 +0700

    form dang nhap hien thi loi chao

--- greeting/routes/web.php
+++ greeting/routes/web.php
@@ -15,10 +15,17 @@ use App\Http\Controllers\TestController;
 */
 
 
-Route::get('/greeting/{name?}',  function ($name = null) {
-    if ($name) {
-        echo 'Hello ' . $name . '!';
-    } else {
-        echo 'Hello World !';
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/login', function (Illuminate\Http\Request $request) {
+    if (
+        $request->username == 'admin'
+        && $request->password == 'admin'
+    ) {
+        return view('welcome_admin');
     }
+
+    return view('login_error');
 });
