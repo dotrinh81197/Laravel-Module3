72464681ce2d96ae279b8c4a4eeb13e97334cd5e
dotrinh81197 <dotrinh81197@users.noreply.github.com>
2021-03-25 08:17:53 +0700

    task_manager

---
+++ task_manager/resources/views/modules/customer/index.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Task Management</title>
+</head>
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>STT</th>
+                <th>Họ và tên</th>
+                <th>Số điện thoại</th>
+                <th>Email</th>
+                <th>Thao tác</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Nguyễn Văn A</td>
+                <td>01234567890</td>
+                <td>email.test@mail.com</td>
+                <td>
+                   <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa </a>
+                </td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Nguyễn Văn B</td>
+                <td>01234567890</td>
+                <td>email.test@mail.com</td>
+                <td>
+                    <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+                </td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Nguyễn Văn C</td>
+                <td>01234567890</td>
+                <td>email.test@mail.com</td>
+                <td>
+                    <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+                </td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Nguyễn Văn D</td>
+                <td>01234567890</td>
+                <td>email.test@mail.com</td>
+                <td>
+                    <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+                </td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Nguyễn Văn E</td>
+                <td>01234567890</td>
+                <td>email.test@mail.com</td>
+                <td>
+                    <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+                </td>
+            </tr>
+        </tbody>
+      </table>
+</body>
+</html>
