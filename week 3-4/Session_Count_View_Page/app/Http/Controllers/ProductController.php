d0e93dec67acac51f8c926033f4f2ae834e9e021
dotrinh81197 <dotrinh81197@users.noreply.github.com>
2021-04-06 09:12:57 +0700

    'session_count_view_page'

---
+++ week 3-4/Session_Count_View_Page/app/Http/Controllers/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('index', compact('products'));
+    }
+
+    public function show(Request $request)
+    {
+        $id = $request->id;
+        $productKey = 'product_' . $id;
+
+        // Kiểm tra Session của sản phẩm có tồn tại hay không.
+        // Nếu không tồn tại, sẽ tự động tăng trường view_count lên 1 đồng thời tạo session lưu trữ key sản phẩm.
+        if (!Session::has($productKey)) {
+            Product::where('id', $id)->increment('view_count');
+            Session::put($productKey, 1);
+        } else {
+            Product::where('id', $id)->increment('view_count');
+        }
+
+        // Sử dụng Eloquent để lấy ra sản phẩm theo id
+        $product = Product::find($id);
+
+        // Trả về view
+        return view('detail', compact(['product']));
+    }
+}
